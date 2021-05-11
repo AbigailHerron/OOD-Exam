ebfc08cdc41dcd4da3372fa6a7cb90206852ef35
AbigailHerron <abigailherron@hotmail.com>
2021-05-11 09:53:17 +0100

    Create Game.cs

---
+++ AbigailHerron_S00200536/AbigailHerron_S00200536/classes/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbigailHerron_S00200536
+{
+    public class Game
+    {
+        /*PROPERTIES ------------------------------------------------------------------------------------------------------*/
+        public int GameID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public string Game_Image { get; set; }
+        public int Metacritic_Score { get; set; }
+        public decimal Price { get; set; }
+
+    }// end Game class
+
+
+}// end AbigailHerron_S00200536 namespace
